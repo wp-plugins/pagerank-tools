38377ae21aa79d752f36b525f17a4cd71d42730e
mahype <mahype@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-15 10:46:33 +0000

    First version
    git-svn-id: https://plugins.svn.wordpress.org/pagerank-tools/trunk@300336 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,42 @@
+=== Pagerank tools ===
+Contributors: mahype
+Tags: pagerank,page rank,rank,siterank,site rank,pr,stats,statistics,seo,google
+Requires at least: 2.9.x
+Tested up to: 3.0.1
+Stable tag:  0.1
+
+View and monitor pagerank of your wordpress sites. 
+
+== Description ==
+This plugin requests the PageRank of your wordpress urls and stores the pageranks in the database.
+
+Requests to google where made in moment of visitors request of a site. There is no cron option, cause of better random requests. 
+
+<h4>Function overview</h4>
+
+<ul>
+   <li>Overview of all urls with pageranks</li>
+   <li>Storing PageRank history of all sites</li>
+   <li>Random requests of PageRanks</li>
+</ul>
+
+PageRank is a trademark of Google, Inc.
+
+== Installation ==
+1. Upload 'Pagerank tools' to the '/wp-content/plugins/' directory<br>
+2. Activate 'Pagerank tools' on plugin page.
+
+
+That's it, have fun!
+
+== Screenshots ==
+
+1. **URL overview**
+1. **Settings**
+
+== Changelog ==
+<h4>0.1</h4>
+<ul>
+<li>First version of Pagerank tools.</li>
+</ul>
+
---
+++ functions.inc.php
@@ -0,0 +1,130 @@
+<?php
+
+function fetch_pr(){
+	global $wpdb;
+	global $prtools_url_table;
+	global $prtools_pr_table;
+	
+	$prtools_settings=get_option('pagerank_tools_settings');
+	
+	$lastupdate=time()-$prtools_settings['fetch_interval']*60;
+	$lastupdate_url=time()-$prtools_settings['fetch_url_interval']*24*60*60;
+	$lastupdate_url_new=time()-$prtools_settings['fetch_url_interval_new']*24*60*60;
+
+	
+	$sql="SELECT * FROM ".$prtools_url_table." ORDER BY lastupdate DESC LIMIT 0,1";
+	
+	$prtools_last = $wpdb->get_row($sql);
+	
+	/*
+	echo "LU: ".$prtools_last->lastupdate."<br>";
+	$s=time()-$prtools_last->lastupdate;
+	echo "S: ".$s."<br>";
+	echo "T: ".time()."<br>";
+	*/
+	
+	/*
+	* Checking time of last pr request
+	***************************************/
+	if($prtools_last->lastupdate<$lastupdate){
+		
+		// Getting sites which are updatable by settings
+		$sql="SELECT * FROM ".$prtools_url_table." WHERE lastupdate<".$lastupdate_url." OR (lastupdate<".$lastupdate_url_new." AND pr=-1) ORDER BY lastupdate LIMIT 0,".$prtools_settings['fetch_num'];
+		
+		$prtools_rows=$wpdb->get_results($sql);
+		
+		/*
+		* Checking pageranks
+		***************************************/	
+		foreach($prtools_rows AS $prtools_row){
+			$pr=getpagerank($prtools_row->url);
+			if($pr==""){$pr=-1;}
+			
+			// echo "PR: ".$pr." (".$prtools_row->url.")<br>";
+			
+			$wpdb->update($prtools_url_table, array('lastupdate'=>time(),'pr'=>$pr), array('url'=>$prtools_row->url));
+			if($pr>-1){
+				$wpdb->insert($prtools_pr_table,array('entrydate'=>time(),'url'=>$prtools_row->url,'pr'=>$pr));
+			}		
+		}
+	}
+
+}
+
+function update_url_table(){
+	global $wpdb;
+	global $prtools_url_table;
+	
+	$urls=wp_get_post_urls();
+	
+	foreach($urls AS $url){
+		$prtools_rows = $wpdb->get_results( "SELECT * FROM ".$prtools_url_table." WHERE url='".$url."'");
+		if(count($prtools_rows)==0){
+			$wpdb->insert($prtools_url_table,array('entrydate'=>time(),'url_type'=>'post','url'=>$url,'pr'=>-1));
+		}
+	}	
+	
+	$urls=wp_get_page_urls();
+	
+	foreach($urls AS $url){
+		$prtools_rows = $wpdb->get_results( "SELECT * FROM ".$prtools_url_table." WHERE url='".$url."'");
+		if(count($prtools_rows)==0){
+			$wpdb->insert($prtools_url_table,array('entrydate'=>time(),'url_type'=>'page','url'=>$url,'pr'=>-1));
+		}
+	}
+	
+	$urls=wp_get_cat_urls();
+	
+	foreach($urls AS $url){
+		$prtools_rows = $wpdb->get_results( "SELECT * FROM ".$prtools_url_table." WHERE url='".$url."'");
+		if(count($prtools_rows)==0){
+			$wpdb->insert($prtools_url_table,array('entrydate'=>time(),'url_type'=>'category','url'=>$url,'pr'=>-1));
+		}
+	}
+	
+	$urls=wp_get_tag_urls();
+	
+	foreach($urls AS $url){
+		$prtools_rows = $wpdb->get_results( "SELECT * FROM ".$prtools_url_table." WHERE url='".$url."'");
+		if(count($prtools_rows)==0){
+			$wpdb->insert($prtools_url_table,array('entrydate'=>time(),'url_type'=>'tag','url'=>$url,'pr'=>-1));
+		}
+	}
+	
+	$home=get_bloginfo("url").'/';
+	$prtools_rows = $wpdb->get_results( "SELECT * FROM ".$prtools_url_table." WHERE url='".$home."'");
+	if(count($prtools_rows)==0){
+		$wpdb->insert($prtools_url_table,array('entrydate'=>time(),'url_type'=>'home','url'=>$home,'pr'=>-1));
+	}
+}
+
+function prtools_css(){
+	echo "<link rel=\"stylesheet\" href=\"".get_option('siteurl')."/wp-content/plugins/pagerank-tools/ui/styles.css\" type=\"text/css\" media=\"screen\" />";
+}
+if(!function_exists('ajaxui')){
+	function ajaxui(){
+		echo "<link href=\"http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.0/themes/base/jquery-ui.css\" rel=\"stylesheet\" />";
+		echo "<script src= \"https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js\" type=\"text/javascript\"></script>\n";
+		echo "<script src=\"https://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/jquery-ui.min.js\" type=\"text/javascript\"></script>\n";
+		?>
+<script type="text/javascript">
+	function openExpandyGroup( groupId )
+	{
+		var eOpen = document.getElementById(groupId + '_open');
+		eOpen.style.display = 'block';
+		var eClosed = document.getElementById(groupId + '_closed');
+		eClosed.style.display = 'none';
+	}
+	function closeExpandyGroup( groupId )
+	{
+		var eOpen = document.getElementById(groupId + '_open');
+		eOpen.style.display = 'none';
+		var eClosed = document.getElementById(groupId + '_closed');
+		eClosed.style.display = 'block';
+	}
+</script>    
+		<?
+	}
+}
+
+?>
